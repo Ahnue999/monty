1324aab82c118efa7e90a39d9478965cea5f4321
Ahnue999 <ahnue200572@gmail.com>
2023-12-12 21:35:40 -0800

    committed

--- opcodes2.c
+++ opcodes2.c
@@ -57,3 +57,78 @@ void nop_opcode(__attribute__((unused)) stack_t **top, unsigned int line)
 {
 	(void)line;
 }
+
+
+
+
+/**
+  * sub_opcode - subs the top two elements of a stack.
+  * @top: a pointer to the top of the stack.
+  * @line: line number.
+  *
+  * Return: void.
+  */
+void sub_opcode(stack_t **top, unsigned int line)
+{
+	int temp;
+
+	if (isempty(*top) || !(*top)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = pop(top);
+	(*top)->n -= temp;
+}
+
+
+
+/**
+  * div_opcode - divides the top two elements of a stack.
+  * @top: a pointer to the top of the stack.
+  * @line: line number.
+  *
+  * Return: void.
+  */
+void div_opcode(stack_t **top, unsigned int line)
+{
+	int temp;
+
+	if (isempty(*top) || !(*top)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = pop(top);
+	if (!temp)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
+	(*top)->n /= temp;
+}
+
+
+
+/**
+  * mul_opcode - multiplies the top two elements of a stack.
+  * @top: a pointer to the top of the stack.
+  * @line: line number.
+  *
+  * Return: void.
+  */
+void mul_opcode(stack_t **top, unsigned int line)
+{
+	int temp;
+
+	if (isempty(*top) || !(*top)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = pop(top);
+	(*top)->n *= temp;
+}
